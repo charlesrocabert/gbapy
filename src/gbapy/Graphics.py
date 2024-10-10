b690be3115089142f5bb4c25bd289a175e0777b3
Charles Rocabert <charles.rocabert@gmail.com>
2024-10-10 15:53:02 +0200

    Re-organizing the repository as a Pypi Python package

--- src/gbapy/Graphics.py
+++ src/gbapy/Graphics.py
@@ -2,13 +2,25 @@
 # coding: utf-8
 
 #***********************************************************************
-# Copyright © 2024 Charles Rocabert, Furkan Mert
-# Web: https://github.com/charlesrocabert/GBA_Evolution
+# gbapy
+# -----
+# Growth balance analysis for Python.
+# 
+# Copyright © 2023-2024 Charles Rocabert, Furkan Mert
+# Web: https://github.com/charlesrocabert/gbapy
 #
-# Graphics.py
-# -----------
-# Definition of various plot functions.
-# (LOCAL SCRIPT)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #***********************************************************************
 
 import os
