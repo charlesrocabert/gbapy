4687c492ee8073e3984fa21c7012b57ebd8b4c7e
Charles Rocabert <charles.rocabert@gmail.com>
2024-10-18 13:25:05 +0200

    Updated gitignore

--- scripts/redump.sh
+++
@@ -1,6 +0,0 @@
-python csv_to_gba.py -name A -LP -optimums
-python csv_to_gba.py -name B -LP -optimums
-python csv_to_gba.py -name C -LP -optimums
-python csv_to_gba.py -name D -LP -optimums
-python csv_to_gba.py -name EC12b -LP -optimums
-
