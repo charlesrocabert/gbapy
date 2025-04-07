2157384e8c3de27239bef8b07f1efb941edb2c76
Charles Rocabert <charles.rocabert@gmail.com>
2025-04-07 15:30:37 +0200

    Added a script to export tutorials

---
+++ tutorials/MMSYN_tutorial/export_tutorials.sh
@@ -0,0 +1,7 @@
+cp MMSYN_tutorial_introduction.ipynb ../../../MMSYN_tutorial/.
+cp MMSYN_tutorial_1.ipynb ../../../MMSYN_tutorial/.
+cp MMSYN_tutorial_2.ipynb ../../../MMSYN_tutorial/.
+cp MMSYN_tutorial_3.ipynb ../../../MMSYN_tutorial/.
+cp MMSYN_tutorial_4.ipynb ../../../MMSYN_tutorial/.
+cp MMSYN_tutorial_5.ipynb ../../../MMSYN_tutorial/.
+
