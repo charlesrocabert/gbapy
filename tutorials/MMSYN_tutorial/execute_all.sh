104ac5e554363856288c5b4ff4db2060c299606d
Charles Rocabert <charles.rocabert@gmail.com>
2025-06-24 15:28:05 +0200

    Minor MMSYN tutorial updates

--- tutorials/MMSYN_tutorial/execute_all.sh
+++ tutorials/MMSYN_tutorial/execute_all.sh
@@ -4,4 +4,5 @@ jupyter execute MMSYN_tutorial_2.ipynb
 jupyter execute MMSYN_tutorial_3.ipynb
 jupyter execute MMSYN_tutorial_4.ipynb
 jupyter execute MMSYN_tutorial_5.ipynb
+jupyter execute MMSYN_tutorial_6.ipynb
 
