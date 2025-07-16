90e1c7cbf000dd6204041fb90d13dbdc9f00228c
Charles Rocabert <charles.rocabert@gmail.com>
2025-07-16 10:16:27 +0200

    Improved licensing

--- gba/__init__.py
+++ gba/__init__.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-#***********************************************************************
-# GBApy (Growth Balance Analysis for Python)
-# Copyright © 2024-2025 Charles Rocabert
+#*******************************************************************************
+# gbapy (Growth Balance Analysis for Python)
 # Web: https://github.com/charlesrocabert/gbapy
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#***********************************************************************
+# 
+# MIT License
+# 
+# Copyright © 2024-2025 Charles Rocabert. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#*******************************************************************************
 
 """
 Filename: __init__.py
 Author: Charles Rocabert
 Date: 2025-05-03
 Description:
-    __init__ file of the GBApy module.
-License: GNU General Public License v3.0
-Copyright: © 2024-2025 Charles Rocabert
+    __init__ file of the gbapy module.
+License: MIT License
+Copyright: © 2024-2025 Charles Rocabert. All rights reserved.
 """
 
 from gba import (
--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-#***********************************************************************
-# GBApy (Growth Balance Analysis for Python)
-# Copyright © 2024-2025 Charles Rocabert
+#*******************************************************************************
+# gbapy (Growth Balance Analysis for Python)
 # Web: https://github.com/charlesrocabert/gbapy
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#***********************************************************************
+# 
+# MIT License
+# -----------
+# 
+# Copyright © 2024-2025 Charles Rocabert. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#*******************************************************************************
 
 
-"""GBApy (Growth Balance Analysis for Python).
+"""gbapy (Growth Balance Analysis for Python).
 
 See:
 https://github.com/charlesrocabert/gbapy
@@ -39,8 +48,8 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
 	name                          = "gba",
 	version                       = "0.0.1",
-	license                       = "GNU General Public License v3 (GPLv3)",
-	description                   = "GBApy (Growth Balance Analysis for Python)",
+	license                       = "MIT License",
+	description                   = "gbapy (Growth Balance Analysis for Python)",
 	long_description              = long_description,
 	long_description_content_type = "text/markdown",
 	url                           = "https://github.com/charlesrocabert/gbapy",
@@ -50,7 +59,7 @@ setup(
 	classifiers = [
 		"Development Status :: 4 - Beta",
 		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 		"Intended Audience :: Science/Research",
 		"Topic :: Scientific/Engineering :: Mathematics",
