15308355243741d854ea758d3a56c2e7505e4048
Charles Rocabert <charles.rocabert@gmail.com>
2025-09-11 15:06:17 +0200

    Switching back to GPL v3 license

--- examples/generate_toy_model.py
+++ examples/generate_toy_model.py
@@ -2,10 +2,10 @@
 # coding: utf-8
 
 #***********************************************************************
-# GBApy (Growth Balance Analysis for Python)
-# Copyright © 2024-2025 Charles Rocabert, Furkan Mert
+# gbapy (Growth Balance Analysis for Python)
 # Web: https://github.com/charlesrocabert/gbapy
-#
+# Copyright © 2024-2025 Charles Rocabert.
+# 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -17,7 +17,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #***********************************************************************
 
 """
@@ -26,7 +26,7 @@ Author: Charles Rocabert
 Date: 2024-09-07
 Description:
     Executable to generate.
-License: GNU General Public License v3.0
+License: GNU General Public License v3 (GPLv3)
 Copyright: © 2024-2025 Charles Rocabert, Furkan Mert
 """
 
--- gba/Model.py
+++ gba/Model.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#*******************************************************************************
+#***********************************************************************
 # gbapy (Growth Balance Analysis for Python)
 # Web: https://github.com/charlesrocabert/gbapy
+# Copyright © 2024-2025 Charles Rocabert.
 # 
-# MIT License
-# 
-# Copyright © 2024-2025 Charles Rocabert. All rights reserved.
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#*******************************************************************************
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#***********************************************************************
 
 """
 Filename: Model.py
@@ -34,8 +26,8 @@ Author: Charles Rocabert, Furkan Mert
 Date: 2024-10-22
 Description:
     Model class of the gbapy module.
-License: MIT License
-Copyright: © 2024-2025 Charles Rocabert, Furkan Mert. All rights reserved.
+License: GNU General Public License v3 (GPLv3)
+Copyright: © 2024-2025 Charles Rocabert, Furkan Mert.
 """
 
 import os
